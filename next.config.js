42a68108cf311d04be4702ccc30ffb59d8c27386
Mahmoud <mahmoud.mamdoh0812@gmail.com>
2025-08-02 19:19:26 +0300

    all done

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['github.com', 'linkedin.com'],
-  },
+  }
 }
 
 module.exports = nextConfig 
